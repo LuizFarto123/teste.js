2a3b074532b8907f80f00556cc0477336d2df9db
LuicitosSigmitas <luicitosoares.domfarto@gmail.com>
2025-07-28 09:48:31 -0300

    segundo commit sigma

---
+++ aula0.js
@@ -0,0 +1,7 @@
+const prompt = require("prompt-sync")();
+
+let nome = prompt("Digite o seu nome: ");
+let idade = prompt("Qual sua idade: ");
+
+console.log("Bem vindo ", nome);
+console.log("Sua idade ", idade);
