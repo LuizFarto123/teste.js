37a7d9577843f1cdf43e412666d343c7f5ac915b
LuizFarto123 <luicitosoares.domfarto@gmail.com>
2025-07-29 09:29:27 -0300

    1a a 1g

--- aula0.js
+++ aula0.js
@@ -1,7 +1,57 @@
 const prompt = require("prompt-sync")();
 
-let nome = prompt("Digite o seu nome: ");
-let idade = prompt("Qual sua idade: ");
+//let numeroA = Number(prompt("Digite um numero: "));//let nome = prompt("Digite o seu nome: ");
+//let numeroB = Number(prompt("Digite um numero: "));//let idade = prompt("Qual sua idade: ");
+//let numeroC = Number(prompt("Digite um numero: "));//let altura = prompt("Qual sua altura ");
+//let preço = 120
+//let distancia = Number(prompt("Quntos kms tem dua viajem "));
+//let consumo = Number(prompt("Qual o consumo do carro km/l "));
+//let valor = Number(prompt("Quanto o grupo gastou"));
+//let quantidade = Number(prompt("quantas pessoas foram"));
+//let numero = Number(prompt("Digite um numero: "));
 
-console.log("Bem vindo ", nome);
-console.log("Sua idade ", idade);
+
+//if(numero % 3 == 0){
+    //console.log("É multiplo de 3");
+//}else{
+    //console.log("não é multiplo de 3");
+//}
+//console.log("fica ", valor/quantidade, "para cada");   
+//console.log("você precisa de ", distancia / consumo, "l de combustivel");   
+//console.log("Condesconto ficou ", (preço-(preço * 0,15)+10));
+//console.log("Meu nome é, ", nome, "tenho ", idade, "anos e", altura, "m de altura");
+//console.log(numeroA + numeroB);
+//console.log(numeroA % numeroB);
+//console.log(numeroA * numeroB);
+//console.log(numeroA - numeroB);
+//console.log(numeroA / numeroB);
+//console.log(numeroA + numeroC);
+//console.log(numeroA % numeroC);
+//console.log(numeroA * numeroC);
+//console.log(numeroA - numeroC);
+//console.log(numeroA / numeroC);
+//console.log(numeroB + numeroC);
+//console.log(numeroB % numeroC);
+//console.log(numeroB * numeroC);
+//console.log(numeroB - numeroC);
+//console.log(numeroB / numeroC);
+//console.log(numeroB + numeroA);
+//console.log(numeroB % numeroA);
+//console.log(numeroB * numeroA);
+//console.log(numeroB - numeroA);
+//console.log(numeroB / numeroA);
+//console.log(numeroC + numeroA);
+//console.log(numeroC % numeroA);
+//console.log(numeroC * numeroA);
+//console.log(numeroC - numeroA);
+//console.log(numeroC / numeroA);
+//console.log(numeroC + numeroB);
+//console.log(numeroC % numeroB);
+//console.log(numeroC * numeroB);
+//console.log(numeroC - numeroB);
+//console.log(numeroC / numeroB);
+
+
+
+
+
