38d6d4ff64cc1e6fe6e6a717c63954511c28e103
LuizFarto123 <luicitosoares.domfarto@gmail.com>
2025-07-31 16:16:31 -0300

    Add files via upload

---
+++ Trabalho senai/Todasastvs.js
@@ -0,0 +1,124 @@
+const prompt = require("prompt-sync")();
+
+//let numeroA = Number(prompt("Digite um numero: "));//let nome = prompt("Digite o seu nome: ");
+//let numeroB = Number(prompt("Digite um numero: "));//let idade = prompt("Qual sua idade: ");
+//let numeroC = Number(prompt("Digite um numero: "));//let altura = prompt("Qual sua altura ");
+//let preço = 120
+//let distancia = Number(prompt("Quntos kms tem dua viajem "));
+//let consumo = Number(prompt("Qual o consumo do carro km/l "));
+//let valor = Number(prompt("Quanto o grupo gastou"));
+//let quantidade = Number(prompt("quantas pessoas foram"));
+//let numero = Number(prompt("Digite um numero: "));
+
+
+//if(numero % 3 == 0){
+    //console.log("É multiplo de 3");
+//}else{
+    //console.log("não é multiplo de 3");
+//}
+//console.log("fica ", valor/quantidade, "para cada");   
+//console.log("você precisa de ", distancia / consumo, "l de combustivel");   
+//console.log("Condesconto ficou ", (preço-(preço * 0,15)+10));
+//console.log("Meu nome é, ", nome, "tenho ", idade, "anos e", altura, "m de altura");
+//console.log(numeroA + numeroB);
+//console.log(numeroA % numeroB);
+//console.log(numeroA * numeroB);
+//console.log(numeroA - numeroB);
+//console.log(numeroA / numeroB);
+//console.log(numeroA + numeroC);
+//console.log(numeroA % numeroC);
+//console.log(numeroA * numeroC);
+//console.log(numeroA - numeroC);
+//console.log(numeroA / numeroC);
+//console.log(numeroB + numeroC);
+//console.log(numeroB % numeroC);
+//console.log(numeroB * numeroC);
+//console.log(numeroB - numeroC);
+//console.log(numeroB / numeroC);
+//console.log(numeroB + numeroA);
+//console.log(numeroB % numeroA);
+//console.log(numeroB * numeroA);
+//console.log(numeroB - numeroA);
+//console.log(numeroB / numeroA);
+//console.log(numeroC + numeroA);
+//console.log(numeroC % numeroA);
+//console.log(numeroC * numeroA);
+//console.log(numeroC - numeroA);
+//console.log(numeroC / numeroA);
+//console.log(numeroC + numeroB);
+//console.log(numeroC % numeroB);
+//console.log(numeroC * numeroB);
+//console.log(numeroC - numeroB);
+//console.log(numeroC / numeroB);
+
+///////////////////////////////////////////////////////////////////////////////////////////////////
+
+// let nota = Number(prompt("\nQual sua nota ?\n")), nota1 = Number(prompt("\nQual sua nota ?\n")), nota2 = Number(prompt("\nQual sua nota ?\n"))
+
+// if((nota + nota1 + nota2)/3 >= 7){
+//     console.log("\nAprovado!!!\n")
+    
+// }else{
+//     console.log("\nReprovado!!!\n")
+// }
+
+// let numero = Number(prompt("\ndigite um numero\n"))
+
+// if(numero%2 == 0){
+//    console.log("par")
+//     ("par")
+// }else{
+//     console.log("impar")
+// }
+
+// let idade = Number(prompt("\nQuantos anos você tem?\n")) 
+
+// if(idade <= 12){
+//     console.log("Criança")
+// }else if(idade < 18){
+//     console.log("Adolecente")
+// }else if(idade < 65){
+//     console.log("Adulto")
+// }else if(idade >= 65){
+//     console.log("Idoso")
+// }
+
+// let idade = Number(prompt("Digite sua idade: "));
+// let sexo = prompt("Digite seu sexo (M/F): ").toUpperCase();
+
+// if (idade >= 18 && idade <= 29 && sexo === "M") {
+//     console.log("Seu alistamento é obrigatório.");
+// } else if (idade >= 18 && idade <=29 && sexo === "F") {
+//     console.log("Seu alistamento é opcional.");
+// } else if (idade >=30 && sexo === "M") {
+//     console.log("Você não é obrigado a se alistar, mas deve obter o Certificado de Dispensa.");
+// }else {
+//     console.log("Você não precisa se alistar.");
+// }
+
+
+// let numeros = new Array(5);
+
+// numeros[0] = Number(prompt("Digite o número1: "));
+// numeros[1] = Number(prompt("Digite o número2: "));
+// numeros[2] = Number(prompt("Digite o número3: "));
+// numeros[3] = Number(prompt("Digite o número4: "));
+// numeros[4] = Number(prompt("Digite o número5: "));
+
+// let maiorNumero = numeros[0];
+
+// for(let i = 1; i < 5; i++){
+//     if(numeros[i] > maiorNumero){
+//         maiorNumero = numeros[i];
+//     }
+// }
+
+// console.log(`O maior número entre os digitados foi o: `,maiorNumero)
+
+// let soma = 0;
+
+// for(let i = 11; i <= 30; i += 2){
+//     soma += i;
+// }
+
+// console.log(`Soma de todos os números ímpares maiores que 10 e menores que 30: `, soma);
