0fb1fe1882f61cdcd6df24f03bd8a5fbfb6ed10c
Borges <you@example.com>
2025-08-06 11:29:03 -0300

    skibidi

---
+++ Trabalho senai/Matriz.js
@@ -0,0 +1,10 @@
+let alunos = [
+    ['João', 10],
+    ['José', 6],
+    ['Maria', 7.5]
+]
+
+for(let i = 0; i < alunos.length; i++){
+
+    console.log(`${alunos[i][0]} tirou ${alunos[i][1]}`);
+}
--- Trabalho senai/atv4-b.js
+++ Trabalho senai/atv4-b.js
@@ -1,3 +1,45 @@
-let Alunos = [], Notas = [] 
+let alunos = [
+    ['Larissa', 10],
+    ['Lucas', 2],
+    ['Rafael', 5],
+    ['Otávio', 8],
+    ['Luiz', 10],
+    ['Daniel', 9.5],
+    ['Eduardo', 7],
+    ['Afonso', 9.2],
+    ['Giovane', 0],
+    ['Pedro', 1],
+    ['Gabriel', 0.5],
+    ['Cleber', 7.5],
+    ['Antonio', 0.25],
+    ['Antunes', 8],
+    ['Kleyton', 5],
+    ['Cleyton', 5.5],
+    ['Claython', 6],
+    ['Claython', 6.5],
+    ['Klaython', 7],
+    ['Cleython', 7.5],
+    ['Kleython', 8],
+    ['Mateus', 6.25],
+    ['Matheus', 7],
+    ['Walter', 1.5],
+    ['Artur', 6],
+    ['Arthur', 4],
+    ['Lucca', 3],
+    ['Valentim', 3.5],
+    ['Tiago', 3.4],
+    ['Thiago', 4.5],
+    ['Maikon', 5],
+    ['Maykon', 7],
+    ['Caio', 7.5],
+    ['Julio', 3],
+    ['Felipe', 2.5]
+]
 
-for()
+for(let i = 0; i <= alunos.length; i++){
+    if(alunos[i][1] >= 7){
+        console.log(`${alunos[i][1]} passou`)
+    }else{
+        console.log(`${alunos[i][0]} reprovou`)
+    }
+}
